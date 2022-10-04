55af1b8fcab6067e8a6ebe15fbb8adcf309252d4
logan-eller <logan.eller@gmail.com>
2022-10-04 11:16:44 -0400

    Reset level 1 works, time to beat 35
    if beaten in > 35 seconds then only option is to replay, otherwise, click to play again, enter to move onto city level

---
+++ Assets/FinishLevel1.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLevel1 : MonoBehaviour
+{
+	//A reference to the game manager
+	public GameManager gameManager;
+
+	// When an object enters the finish zone, let the
+	// game manager know that the current game has ended
+	void OnTriggerEnter(Collider other)
+	{
+		gameManager.RestartLev1();
+	}
+}
+
