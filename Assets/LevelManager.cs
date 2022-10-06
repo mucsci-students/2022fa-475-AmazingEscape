201add5187a57abeccf8354469d249127ef2cacd
Justin Stevens <jmms5757@gmail.com>
2022-10-06 15:45:12 -0400

    worked on settings menu

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -5,8 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+
+    public GameObject canvas;
+    public GameObject mainMenu;
+    public GameObject backgroundImg;
+    public GameObject eventSystems;
+
     public void loadScene()
     {
+        DontDestroyOnLoad (canvas);
+        DontDestroyOnLoad (eventSystems);
         SceneManager.LoadScene(sceneName: "Hallway Level");
+        mainMenu.SetActive(false);
+        backgroundImg.SetActive(false);
     }
 }
